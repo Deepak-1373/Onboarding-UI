b0a2d5c12399e1052414ccf4ce06095721584972
Deepak Kamat <dkamat.001@gmail.com>
2022-06-21 19:34:36 +0530

    feat: add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Input } from "./component";
+import { Button, Input } from "./component";
 
 function App() {
   return (
     <div className="app">
       <h1>Onboarding-UI</h1>
       <Input type="text" placeholder="Steve Jobs" />
+      <Button text="Create Workspace" />
     </div>
   );
 }
