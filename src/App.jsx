0e28cfcf1f73525a034e80cb4d49134cefeb730f
Deepak Kamat <dkamat.001@gmail.com>
2022-06-21 19:21:16 +0530

    feat: add input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Input } from "./component";
 
 function App() {
   return (
     <div className="app">
       <h1>Onboarding-UI</h1>
+      <Input type="text" placeholder="Steve Jobs" />
     </div>
   );
 }
