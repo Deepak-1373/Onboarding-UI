8aa140fdc85ca9a967dbfc178f0713eea8f7deb3
Deepak Kamat <dkamat.001@gmail.com>
2022-06-21 22:48:03 +0530

    refactor: change layout for label and input

--- src/component/Input/Input.jsx
+++ src/component/Input/Input.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-export const Input = ({ id, type, placeholder }) => {
+export const Input = ({
+  id = "",
+  type = "",
+  placeholder = "",
+  labelText = "",
+  labelData = "",
+}) => {
   return (
-    <input
-      required
-      id={id}
-      className={styles.input}
-      type={type}
-      placeholder={placeholder}
-    />
+    <div className="pb-4">
+      <label className="text-base" htmlFor={id}>
+        {labelText} <span>{labelData}</span>
+        <input
+          required
+          id={id}
+          className={styles.input}
+          type={type}
+          placeholder={placeholder}
+        />
+      </label>
+    </div>
   );
 };
