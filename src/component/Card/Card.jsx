4008d298469445379043d15baf81b538445ab000
Deepak Kamat <dkamat.001@gmail.com>
2022-06-21 21:21:43 +0530

    feat(style): change font size of title and subtitle

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -3,10 +3,10 @@ import styles from "./Card.module.css";
 
 export const Card = ({ iconName, title, subtitle }) => {
   return (
-    <div className={`flex-col items-start justify-center`}>
+    <div className={`${styles.card} flex-col items-start justify-center`}>
       <span className="material-symbols-outlined">{iconName}</span>
-      <p>{title}</p>
-      <p>{subtitle}</p>
+      <p className={`${styles.cardTitle} pt-4`}>{title}</p>
+      <p className={`${styles.cardSubTitle} pt-4`}>{subtitle}</p>
     </div>
   );
 };
