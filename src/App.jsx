ec3f2f500f171d31f365ced5f12638a1c3a9107e
Deepak Kamat <dkamat.001@gmail.com>
2022-06-22 10:42:07 +0530

    feat: add outlet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Home, WorkSpace } from "./pages";
+import { Layout } from "./component";
 import "./App.css";
 
 function App() {
   return (
     <div className="app flex-col justify-center items-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/workspace" element={<WorkSpace />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/workspace" element={<WorkSpace />} />
+        </Route>
       </Routes>
     </div>
   );
