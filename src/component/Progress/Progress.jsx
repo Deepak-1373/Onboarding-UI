cbe5a728293deb8fe9bc6a246c1ce0e52b1ba586
Deepak Kamat <dkamat.001@gmail.com>
2022-06-22 18:27:13 +0530

    fix(style): using common styles

--- src/component/Progress/Progress.jsx
+++ src/component/Progress/Progress.jsx
@@ -6,18 +6,18 @@ export const Progress = ({ userProgress = 1 }) => {
   return (
     <div className="flex-col justify-center items-center">
       <img src={Logo} alt="Logo" />
-      <div className={`${styles.container}`}>
+      <div className={`${styles.container} flex items-center justify-between`}>
         {[...Array(4)].map((_, index) => (
           <React.Fragment key={index}>
             <div
-              className={`${styles.item} ${
+              className={`flex items-center justify-center ${styles.item} ${
                 index + 1 <= userProgress ? styles.itemBackground : ``
               }`}
             >
               {index + 1}
             </div>
             {index !== 3 && (
-              <div className={`${styles.separator}`}>
+              <div className={`flex ${styles.separator}`}>
                 <div
                   className={
                     index + 1 <= userProgress ? `${styles.active}` : ``
